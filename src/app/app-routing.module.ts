9850dca7304981def9be0d30c7369814a0f7cef0
soulaichhi <chhibou.soulaimane@gmail.com>
2023-04-24 09:39:23 +0100

    Setup the project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'social-media',
+    loadChildren: () => import('./social-media/social-media.module').then(m => m.SocialMediaModule)
+  },
+  {path: '**', redirectTo: 'social-media'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
